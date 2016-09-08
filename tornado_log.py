050059164ab930d1a41863dd8f003f7760516a7b
lAzUr <jonwing.lee@gmail.com>
2016-09-08 19:38:12 +0800

    tornado_log.py: unicode/str support

--- tornado_log.py
+++ tornado_log.py
@@ -14,6 +14,42 @@ try:
 except ImportError:
     curses = None
 
+#############################
+# unicode resolution
+#############################
+if not isinstance(b'', type('')):
+    def u(s):
+        return s
+    unicode_type = str
+    basestring_type = str
+else:
+    def u(s):
+        return s.decode('unicode_escape')
+    unicode_type = unicode  # noqa
+    basestring_type = basestring  # noqa
+
+
+def to_unicode(value):
+    """Converts a string argument to a unicode string.
+
+    If the argument is already a unicode string or None, it is returned
+    unchanged.  Otherwise it must be a byte string and is decoded as utf8.
+    """
+    if isinstance(value, (unicode_type, type(None))):
+        return value
+    if not isinstance(value, bytes):
+        raise TypeError(
+            "Expected bytes, unicode, or None; got %r" % type(value)
+        )
+    return value.decode("utf-8")
+
+
+def _safe_unicode(s):
+    try:
+        return to_unicode(s)
+    except UnicodeDecodeError:
+        return repr(s)
+
 
 def _stderr_supports_color():
     color = False
@@ -29,10 +65,19 @@ def _stderr_supports_color():
 
 class LogFormatter(logging.Formatter):
     """Log formatter used in Tornado.
-        preserve the color format feature
+
+    Key features of this formatter are:
+
+    * Color support when logging to a terminal that supports it.
+    * Timestamps on every log line.
+    * Robust against str/bytes encoding problems.
+
+    This formatter is enabled automatically by
+    `tornado.options.parse_command_line` (unless ``--logging=none`` is
+    used).
     """
-    DEFAULT_FORMAT = '%(color)s[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d]%(end_color)s %(message)s'
-    DEFAULT_DATE_FORMAT = '%y%m%d %H:%M:%S'
+    DEFAULT_FORMAT = '%(color)s[%(levelname)1.1s %(asctime)s %(name)s|%(funcName)s|%(lineno)d]%(end_color)s %(message)s'
+    DEFAULT_DATE_FORMAT = u'%y%m%d %H:%M:%S'
     DEFAULT_COLORS = {
         logging.DEBUG: 4,  # Blue
         logging.INFO: 2,  # Green
@@ -52,6 +97,10 @@ class LogFormatter(logging.Formatter):
           code
         :arg string datefmt: Datetime format.
           Used for formatting ``(asctime)`` placeholder in ``prefix_fmt``.
+
+        .. versionchanged:: 3.2
+
+           Added ``fmt`` and ``datefmt`` arguments.
         """
         logging.Formatter.__init__(self, datefmt=datefmt)
         self._fmt = fmt
@@ -68,18 +117,18 @@ class LogFormatter(logging.Formatter):
             fg_color = (curses.tigetstr("setaf") or
                         curses.tigetstr("setf") or "")
             if (3, 0) < sys.version_info < (3, 2, 3):
-                fg_color = unicode(fg_color, "ascii")
+                fg_color = unicode_type(fg_color, "ascii")
 
             for levelno, code in colors.items():
-                self._colors[levelno] = unicode(curses.tparm(fg_color, code), "ascii")
-            self._normal = unicode(curses.tigetstr("sgr0"), "ascii")
+                self._colors[levelno] = unicode_type(curses.tparm(fg_color, code), "ascii")
+            self._normal = unicode_type(curses.tigetstr("sgr0"), "ascii")
         else:
             self._normal = ''
 
     def format(self, record):
         try:
             message = record.getMessage()
-            assert isinstance(message, basestring)  # guaranteed by logging
+            assert isinstance(message, basestring_type)  # guaranteed by logging
             # Encoding notes:  The logging module prefers to work with character
             # strings, but only enforces that log messages are instances of
             # basestring.  In python 2, non-ascii bytestrings will make
@@ -96,7 +145,7 @@ class LogFormatter(logging.Formatter):
             # it's worth it since the encoding errors that would otherwise
             # result are so useless (and tornado is fond of using utf8-encoded
             # byte strings whereever possible).
-            record.message = unicode(message)
+            record.message = _safe_unicode(message)
         except Exception as e:
             record.message = "Bad message (%r): %r" % (e, record.__dict__)
 
@@ -107,8 +156,9 @@ class LogFormatter(logging.Formatter):
             record.end_color = self._normal
         else:
             record.color = record.end_color = ''
-
+        # urecord = {unicode_type(k): unicode_type(v) for k, v in record.__dict__.items()}
         formatted = self._fmt % record.__dict__
+        # formatted = self._fmt % urecord
 
         if record.exc_info:
             if not record.exc_text:
@@ -118,7 +168,7 @@ class LogFormatter(logging.Formatter):
             # each line separately so that non-utf8 bytes don't cause
             # all the newlines to turn into '\n'.
             lines = [formatted.rstrip()]
-            lines.extend(unicode(ln) for ln in record.exc_text.split('\n'))
+            lines.extend(_safe_unicode(ln) for ln in record.exc_text.split('\n'))
             formatted = '\n'.join(lines)
         return formatted.replace("\n", "\n    ")
 
